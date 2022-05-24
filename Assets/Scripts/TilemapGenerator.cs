e065185525fdfb7eff10db684385ddeb87473a52
Matt <matt.wilson720@gmail.com>
2022-05-24 15:25:16 -0700

    Setup scrips for geneating Tilemaps and began implementing Lexer and Parser

---
+++ Assets/Scripts/TilemapGenerator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilemapGenerator : MonoBehaviour
+{
+    public GameObject TilemapPrefab;
+    public Sprite BaseTileSprite;
+
+    [HideInInspector]
+    public string DSLInput;
+}
