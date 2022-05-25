fdbf7d201521d104b861b1ce61721f341dcf2779
Matt <matt.wilson720@gmail.com>
2022-05-24 18:54:31 -0700

    Began implementing functions for TilemapGenerator and updated generator command in readme

--- Assets/Scripts/TilemapGenerator.cs
+++ Assets/Scripts/TilemapGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TilemapGenerator : MonoBehaviour
 {
@@ -9,4 +10,24 @@ public class TilemapGenerator : MonoBehaviour
 
     [HideInInspector]
     public string DSLInput;
+
+    // DSL API
+    private Tilemap tilemap;
+
+    public void Canvas(int width, int height) {
+        GameObject tilemapGameObject = Instantiate(TilemapPrefab);
+        tilemap = tilemapGameObject.GetComponentInChildren<Tilemap>();
+    }
+
+    public void Fill(int x, int y, int width, int height, Color32 color32) {
+        Tile tile = ScriptableObject.CreateInstance("Tile") as Tile;
+        tile.sprite = BaseTileSprite;
+        tile.color = color32;
+        for(int i = x; i < x + width; i++){
+            for(int j = y; j < y + height; j++){
+                tilemap.SetTile(new Vector3Int(i,j,0), tile);
+            }
+        }
+
+    }
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Procedural Terrain Generation DSL in Unity
 
 ## Antlr
-Generate C# source files for grammar file using: `antlr4 -Dlanguage=CSharp Grammar.g4`
+Generate C# source files for grammar file using: `antlr4 -Dlanguage=CSharp -visitor TilemapDSLLexer.g4 -o ./gen`
